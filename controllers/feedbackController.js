cb84e5cc608c34e5df2dc89a872e5711df26338d
Croquignolex <gpetitalex10@gmail.com>
2021-07-14 10:36:14 +0100

    Save changes

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -1,5 +1,17 @@
-// feedback messages
+const {mysqlDatabaseConnection} = require("../helpers/mysqlDatabaseHelper");
+
+// GET: feedback messages
 module.exports.messages = async function(req, res) {
-    res.send({message: 'Hello world'});
+    // Query
+    const selectSqlQuery = `
+        SELECT userId, messageId, createdAt, authorId, content, caseId, mediaId
+        FROM feedbacks ORDER BY createdAt DESC
+    `;
+    const mysqlDatabaseResponse = await mysqlDatabaseConnection(selectSqlQuery);
+    // Response
+    if(mysqlDatabaseResponse.status) {
+        const messages = mysqlDatabaseResponse.data;
+        res.send(messages);
+    } else res.status(400).send({message: mysqlDatabaseResponse.message});
 };
 
--- routes/feedback.js
+++ routes/feedback.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const {messages} = require('../controllers/feedbackController');
 const {tokenMiddleware} = require("../middlewares/tokenMiddleware");
 
-router.post('/cases/messages', tokenMiddleware, messages);
+router.get('/cases/messages', tokenMiddleware, messages);
 
 module.exports = router;
