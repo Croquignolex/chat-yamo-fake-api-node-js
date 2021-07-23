3b4aef6a98d1078086793025e2065b6fd68f25f0
Croquignolex <gpetitalex10@gmail.com>
2021-07-23 11:23:40 +0100

    Improve message table

---
+++ migrations/20210714083529-create-messages-table.js
@@ -0,0 +1,46 @@
+let dbm;
+let type;
+let seed;
+
+const TABLE = 'messages';
+
+const TABLE_COLUMNS = ['name'];
+const TABLE_ROWS = [
+  ['Dummy message 1 name'],
+  ['Dummy message 2 name'],
+  ['Dummy message 3 name'],
+];
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.createTable(TABLE, {
+    columns: {
+      id: {type: 'int', unsigned: true, notNull: true, primaryKey: true, autoIncrement: true},
+      name: {type: 'string', notNull: true},
+    },
+    ifNotExists: true
+  }, function (error) {
+    // Insert value
+    if (error) { return; }
+    TABLE_ROWS.forEach((data) => {
+      db.insert(TABLE, TABLE_COLUMNS, data);
+    });
+  });
+};
+
+exports.down = function(db) {
+  return db.dropTable(TABLE);
+};
+
+exports._meta = {
+  "version": 1
+};
