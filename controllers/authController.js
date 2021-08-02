43c3679a0d0b0ccb712f2295f82fbc6d913908f7
Croquignolex <gpetitalex10@gmail.com>
2021-08-02 13:30:14 +0100

    Improve response for backoffice user profile

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,8 +6,9 @@ const {
     AUTH_FAILED,
     USER_LOGGED_OUT,
     FORM_DATA_ERROR,
-    USER_AUTHENTICATED,
+    USER_AUTHENTICATED, USER_NOT_FOUND,
 } = require('../constants/reponseConstants');
+const {mysqlDatabaseConnection} = require("../helpers/mysqlDatabaseHelper");
 
 // Login check
 module.exports.login = async function(req, res) {
@@ -35,5 +36,34 @@ module.exports.login = async function(req, res) {
 // User logout
 module.exports.logout = async function(req, res) {
     res.send({message: USER_LOGGED_OUT});
+}
+
+// User profile
+module.exports.profile = async function(req, res) {
+    // Params data
+    const userId = req.userId;
+    // Query
+    const selectSqlQuery = `SELECT * FROM admins WHERE id = ? LIMIT 1`;
+    const mysqlDatabaseResponse = await mysqlDatabaseConnection(selectSqlQuery, [userId]);
+    // Response
+    if(mysqlDatabaseResponse.status) {
+        if(mysqlDatabaseResponse.data.length > 0) {
+            const user = mysqlDatabaseResponse.data[0];
+            res.send(buildUserResponseData(user));
+        } else res.status(400).send({message: USER_NOT_FOUND});
+    } else res.status(400).send({message: mysqlDatabaseResponse.message});
+};
+
+// Format response
+function buildUserResponseData(user) {
+    return {
+        name: user.name,
+        userId: user.id,
+        phone: user.phone,
+        email: user.email,
+        gender: user.gender,
+        address: user.address,
+        description: user.description,
+    }
 }
 
