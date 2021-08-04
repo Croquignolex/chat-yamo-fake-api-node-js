b32c7d4809d607e59680032f307f9b3806068df5
Croquignolex <gpetitalex10@gmail.com>
2021-08-04 20:13:32 +0100

    Fix media url

--- routes/media.js
+++ routes/media.js
@@ -5,8 +5,8 @@ const {mediaMiddleware} = require("../middlewares/mediaMiddleware");
 const {tokenMiddleware} = require("../middlewares/tokenMiddleware");
 const {userMainImage, uploadImage, chatroomImage} = require('../controllers/mediaController');
 
+router.get('/:mediaId', chatroomImage);
 router.get('/users/:userId/medias/main', userMainImage);
-router.get('/medias/:mediaId/chatrooms', chatroomImage);
 router.post('/chatrooms/:chatroomId/medias', [mediaMiddleware, tokenMiddleware], uploadImage);
 
 module.exports = router;
