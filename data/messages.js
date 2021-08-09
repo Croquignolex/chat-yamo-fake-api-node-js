c0c51276eb13952c1736b363b9aed721b0bf9dbd
Croquignolex <gpetitalex10@gmail.com>
2021-08-09 17:03:49 +0100

    Update message table and publish

--- data/messages.js
+++ data/messages.js
@@ -27,12 +27,39 @@ module.exports.MESSAGES = [
         mediaId: ''
     },
     {
-        messageId: 4,
+        messageId: 5,
         userId: process.env.BACKOFFICE_USER_ID,
         createdAt: new Date().getTime(),
-        authorId: 1,
-        content: "Hello team Chat&Yamo, i can't no more match with other members.",
-        caseId: `1:${process.env.BACKOFFICE_USER_ID}`,
+        authorId: 3,
+        content: "Hello, my name is Ema and i am new.",
+        caseId: `3:${process.env.BACKOFFICE_USER_ID}`,
+        mediaId: ''
+    },
+    {
+        messageId: 6,
+        userId: process.env.BACKOFFICE_USER_ID,
+        createdAt: new Date().getTime(),
+        authorId: 4,
+        content: "Hey! some idiot came and suspend one of my match.",
+        caseId: `4:${process.env.BACKOFFICE_USER_ID}`,
+        mediaId: ''
+    },
+    {
+        messageId: 7,
+        userId: 4,
+        createdAt: new Date().getTime(),
+        authorId: process.env.BACKOFFICE_USER_ID,
+        content: "Hi, please be polite or we will be forced ti delete your account",
+        caseId: `4:${process.env.BACKOFFICE_USER_ID}`,
+        mediaId: ''
+    },
+    {
+        messageId: 8,
+        userId: process.env.BACKOFFICE_USER_ID,
+        createdAt: new Date().getTime(),
+        authorId: 4,
+        content: "Do what u want, stupid.",
+        caseId: `4:${process.env.BACKOFFICE_USER_ID}`,
         mediaId: ''
     }
 ];
