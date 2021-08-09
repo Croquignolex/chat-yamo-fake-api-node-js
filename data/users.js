b1ff3b03b27126ba6285de1c08f916c732d47aa8
Croquignolex <gpetitalex10@gmail.com>
2021-08-09 16:12:22 +0100

    Add some mock users

--- data/users.js
+++ data/users.js
@@ -26,5 +26,33 @@ module.exports.USERS = [
         homeCountry: 'CAMEROON',
         verified: true,
         isPremium: false
+    },
+    {
+        userId: 3,
+        name: 'Ema KONDE',
+        age: 22,
+        gender: 'Female',
+        city: 'DJAMENA',
+        country: 'TCHAD',
+        continent: 'AFRICA',
+        greetingText: 'Welcome Ema',
+        province: 'DJAMENA',
+        homeCountry: 'TCHAD',
+        verified: false,
+        isPremium: false
+    },
+    {
+        userId: 4,
+        name: 'chat_yamo_deleted_account',
+        age: 37,
+        gender: 'Male',
+        city: 'CASA',
+        country: 'MOROCCO',
+        continent: 'AFRICA',
+        greetingText: 'Welcome...',
+        province: 'CASA',
+        homeCountry: 'MOROCCO',
+        verified: false,
+        isPremium: false
     }
 ];
