f01d6767c8056958088de2dc173abeb7217884c3
Croquignolex <gpetitalex10@gmail.com>
2021-08-09 17:23:52 +0100

    Clean unacessary routes

--- routes/media.js
+++ routes/media.js
@@ -5,10 +5,6 @@ const {mediaMiddleware} = require("../middlewares/mediaMiddleware");
 const {tokenMiddleware} = require("../middlewares/tokenMiddleware");
 const {userMainImage, uploadImage, chatroomImage} = require('../controllers/mediaController');
 
-router.get('/:mediaId', chatroomImage);
-
-
-
 router.get('/users/:userId/medias/main', userMainImage);
 router.get('/chatrooms/:chatroomId/media/:mediaId', chatroomImage);
 router.post('/chatrooms/:chatroomId/medias', [mediaMiddleware, tokenMiddleware], uploadImage);
