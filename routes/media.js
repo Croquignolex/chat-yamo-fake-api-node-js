48a17f9de5b2a469e909a3fbe8b9ad4c45d940a9
Croquignolex <gpetitalex10@gmail.com>
2021-10-29 11:45:49 +0100

    update backend route

--- routes/media.js
+++ routes/media.js
@@ -6,7 +6,7 @@ const {tokenMiddleware} = require("../middlewares/accessTokenMiddleware");
 const {userMainImage, uploadImage, chatroomImage} = require('../controllers/mediaController');
 
 router.get('/users/:userId/medias/main', tokenMiddleware, userMainImage);
-router.get('/chatrooms/:chatroomId/media/:mediaId', tokenMiddleware, chatroomImage);
+router.get('/chatrooms/:chatroomId/medias/:mediaId', tokenMiddleware, chatroomImage);
 router.put('/chatrooms/:chatroomId/medias', [mediaMiddleware, tokenMiddleware], uploadImage);
 
 module.exports = router;
