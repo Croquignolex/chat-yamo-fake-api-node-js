4ad90ac934aaced2c9cc41ad71141038f33239ee
Croquignolex <gpetitalex10@gmail.com>
2021-11-23 12:06:03 +0100

    Update mock data

--- data/backoffficeUsers.js
+++ data/backoffficeUsers.js
@@ -1,10 +1,10 @@
 module.exports.BACKOFFICE_USERS = [
     {
-        id: 1, login: 'croquignolex', password: 'croquignolex', username: 'Croquignolex',
+        id: 1, login: 'croquignolex', password: 'croquignolex', username: 'croquignolex@chat-yamo.com',
         firstName: 'Croquignolex', lastName: 'Croquignolex'
     },
     {
-        id: 2, login: 'chat&yamo', password: 'chat&yamo', username: 'Chat&yamo',
+        id: 2, login: 'chat&yamo', password: 'chat&yamo', username: 'Chat&yamo@chat-yamo.com',
         firstName: 'cChat&yamo', lastName: 'Chat&yamo'
     }
 ];
