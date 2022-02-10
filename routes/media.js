e528f4a6e8f0d860ae9aa7f3fb71b1c94cefd6e9
Croquignolex <gpetitalex10@gmail.com>
2022-02-10 16:32:11 +0100

    Fix crash

--- routes/media.js
+++ routes/media.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const {mediaMiddleware} = require("../middlewares/mediaMiddleware");
 const {tokenMiddleware} = require("../middlewares/accessTokenMiddleware");
-const {userMainImage, uploadImage, chatroomImage} = require('../controllers/mediaController');
+const {userMainImage, uploadImage, chatroomImage, userMainImageUrl} = require('../controllers/mediaController');
 
 router.get('/users/:userId/medias/main', tokenMiddleware, userMainImage);
 router.get('/users/:userId/medias/main/info', tokenMiddleware, userMainImageUrl);
