19a99370b9b4f509f08cceaede400908a429e632
Croquignolex <gpetitalex10@gmail.com>
2022-04-05 12:12:51 +0100

    Improve user report faker

--- controllers/feedbacksController.js
+++ controllers/feedbacksController.js
@@ -1,4 +1,6 @@
 const {getCaseByUserIdWithMessages, getCases} = require("../helpers/casesHelper");
+const formCheckerHelper = require("../helpers/formCheckerHelper");
+const {getUserByEmailOrPhone} = require("../helpers/usersHelper");
 
 // GET: User case with messages
 module.exports.userCaseMessages = function(req, res) {
@@ -18,6 +20,16 @@ module.exports.cases = function(req, res) {
     // Response
     if(casesResponse.status) res.send({messages: casesResponse.data});
     else res.status(400).send({message: casesResponse.message});
+};
+
+// POST: Report user profile
+module.exports.report = function(req, res) {
+    // Form data
+    const {feedbackText} = req.body;
+    // Form checker
+    if(formCheckerHelper.requiredChecker(feedbackText)) {
+        res.send({status: true, data: null});
+    } else res.status(400).send({message: "Form data error"});
 };
 
 
--- routes/feedbacks.js
+++ routes/feedbacks.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const {tokenMiddleware} = require("../middlewares/accessTokenMiddleware");
-const {cases, userCaseMessages} = require('../controllers/feedbacksController');
+const {cases, report, userCaseMessages} = require('../controllers/feedbacksController');
 
+router.post('/submit', tokenMiddleware, report);
 router.get('/cases/messages/from', tokenMiddleware, cases);
 router.get('/:userId/case', tokenMiddleware, userCaseMessages);
 
