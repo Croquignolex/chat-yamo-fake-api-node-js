a67982f4e1efc8996b9c208761d43ba460f7d962
Croquignolex <gpetitalex10@gmail.com>
2022-04-06 18:08:25 +0100

    Save & push

--- data/backoffficeUsers.js
+++ data/backoffficeUsers.js
@@ -1,22 +1,22 @@
 module.exports.BACKOFFICE_USERS = [
     {
-        id: 1,
+        id: "1",
         login: 'croquignolex',
         password: 'croquignolex',
         username: 'croquignolex@chat-yamo.com',
         firstName: 'Croquignolex',
         lastName: 'Croquignolex',
-        entityId: 1,
+        entityId: "1",
         roles: ['admin', 'writer']
     },
     {
-        id: 2,
+        id: "2",
         login: 'chat&yamo',
         password: 'chat&yamo',
         username: 'Chat&yamo@chat-yamo.com',
         firstName: 'Chat&yamo',
         lastName: 'Chat&yamo',
-        entityId: 2,
+        entityId: "2",
         roles: ['writer']
     }
 ];
