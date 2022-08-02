2d133b00431c4e8e930f458de518a803e6d9df91
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2022-08-02 06:14:04 +0100

    Fix old image validation routes

--- routes/validation.js
+++ routes/validation.js
@@ -5,7 +5,9 @@ const {tokenMiddleware} = require("../middlewares/accessTokenMiddleware");
 const {deleteUserImage, imagesToBeVerified, verifyImage} = require('../controllers/validationController');
 
 router.get('/users/toBeVerifiedMedia', tokenMiddleware, imagesToBeVerified);
+router.get('/users/allToBeNotedMedias', tokenMiddleware, imagesToBeVerified);
 router.delete('/users/:userId/medias/:mediaId', tokenMiddleware, deleteUserImage);
+router.put('/users/:userId/medias/:mediaId/score/:score', tokenMiddleware, verifyImage);
 router.put('/users/:userId/medias/:mediaId/paths/:mediaPath/verify/:verified', tokenMiddleware, verifyImage);
 
 module.exports = router;
