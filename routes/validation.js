f014a43abba5926f1ff90ba4e9edcc896ee7a1d5
Croquignolex <gpetitalex10@gmail.com>
2022-09-28 05:29:38 +0100

    Fix verifications urls

--- routes/validation.js
+++ routes/validation.js
@@ -5,7 +5,7 @@ const {tokenMiddleware} = require("../middlewares/accessTokenMiddleware");
 const {deleteUserImage, imagesToBeVerified, verifyImage} = require('../controllers/validationController');
 
 router.get('/users/toBeVerifiedMedia', tokenMiddleware, imagesToBeVerified);
-router.get('/users/allToBeNotedMedias', tokenMiddleware, imagesToBeVerified);
+router.get('/users/toBeNotedMedia', tokenMiddleware, imagesToBeVerified);
 router.delete('/users/:userId/medias/:mediaId', tokenMiddleware, deleteUserImage);
 router.put('/users/:userId/medias/:mediaId/score/:score', tokenMiddleware, verifyImage);
 router.put('/users/:userId/medias/:mediaId/paths/:mediaPath/verify/:verified', tokenMiddleware, verifyImage);
