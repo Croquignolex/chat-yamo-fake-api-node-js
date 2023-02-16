2feaadc4de5eee33e9f4ccaaece379a464bec9da
Croquignolex <gpetitalex10@gmail.com>
2023-02-16 05:40:36 +0100

    Update usr profile

--- routes/authentication.js
+++ routes/authentication.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {metadata, block, blockState, souscriptions, history, status} = require('../controllers/usersController');
 const {tokenMiddleware} = require("../middlewares/accessTokenMiddleware");
 
-router.delete('/user/:userId/block', tokenMiddleware, block);
+router.post('/user/:userId/block', tokenMiddleware, block);
 router.get('/backoffice/user/:userId/status', tokenMiddleware, status);
 router.get('/user/:userId/userblockedstate', tokenMiddleware, blockState);
 router.get('/backoffice/user/:userId/metadata', tokenMiddleware, metadata);
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const {details, search} = require('../controllers/usersController');
 const {tokenMiddleware} = require("../middlewares/accessTokenMiddleware");
+const {details, search, status} = require('../controllers/usersController');
 
 router.get('/:userId', tokenMiddleware, details);
+router.post('/:userId', tokenMiddleware, status);
+router.get('/v2/:userId', tokenMiddleware, details);
 router.post('/backoffice/checkuser', tokenMiddleware, search);
 
 module.exports = router;
