c82378bf94f9307bd481e250fbc9034e614ec9f5
Croquignolex <gpetitalex10@gmail.com>
2024-04-11 17:01:28 +0100

    update api

--- routes/feedbacks.js
+++ routes/feedbacks.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const {tokenMiddleware} = require("../middlewares/accessTokenMiddleware");
 const {cases, report, userCaseMessages, roles} = require('../controllers/feedbacksController');
 const {backofficeUsers, deleteBackofficeUser} = require('../controllers/backofficeUsersController');
+const {imagesToBeVerified} = require('../controllers/validationController');
 
 router.get('/backoffice/allroles', roles);
 router.post('/submit', tokenMiddleware, report);
@@ -12,5 +13,7 @@ router.get('/:userId/case', tokenMiddleware, userCaseMessages);
 // Backoffice users
 router.get('/backoffice/:backofficeUserId/allusers', tokenMiddleware, backofficeUsers);
 router.delete('/backoffice/:backofficeUserId/:userId/delete', tokenMiddleware, deleteBackofficeUser);
+router.get('/backoffice/profilesToDoubleCheck', tokenMiddleware, imagesToBeVerified);
+// /feedbacks/backoffice/profilesToDoubleCheck
 
 module.exports = router;
